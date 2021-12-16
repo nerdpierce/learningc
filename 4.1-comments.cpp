8b99ed2b2260d95f55c74aaafa17a217f999ffa5
nerdpierce <nerdpierce1990@gmail.com>
2021-12-16 09:20:50 -0500

    Coming along nicely

---
+++ 4.1-comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+// this is a comment
+// this is another comment
+/* this is a 
+  multi-line comment aka C style */
+// do not use "using namespace std;" for the sake of name clashes!
+  std::cout << "Say hello to the world!";
+}
