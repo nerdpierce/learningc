db3fe4eefc2c32eb09eeba344621c30010439d0e
nerdpierce <nerdpierce1990@gmail.com>
2021-12-30 14:12:46 -0500

    Assignment check in.

--- puzzle3.cpp
+++ puzzle3.cpp
@@ -8,13 +8,22 @@ int main(int argc, char *argv[])
   int *q;
 
 
-  // Heap Allocation 
+  // New Heap Allocation 
 
+  p = new int;
+
+  std::cout << p << std::endl;
+  std::cout << *p << std::endl;
 
   // Assign heap to new heap.
 
+  q = p;
 
-  // Print out 8.
+  // Now assign 8 to p through q and print out 8.
+ 
+  *q = 8;
+
+  std::cout << q << std::endl;
 
 
   // Heap should now hold the value of 9
---
+++ puzzle4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+
+int main(int argc, char *argv[])
+{
+  int *x; 
+
+  // Turn int pointer x into a new heap alloc array with size.
+
+
+  // Create a int with label size with value 3.
+
+
+  // Write a for loop that loops through size.
+
+
+  // Memory management:: delete malloc array and remove
+  // dangling pointer.
+
+
+  return 0;
+}
