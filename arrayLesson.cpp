8eaf7ad491c5f6040b88ed475d919b4e6d45dbb1
nerdpierce <nerdpierce1990@gmail.com>
2022-01-01 12:09:34 -0500

    Add lessons.

---
+++ arrayLesson.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+  // Create an array of 6 numbers.
+
+  int a[6] { 2, 3, 4, 6, 7, 8 };
+
+  // Cout the size of the array.
+  std::cout << sizeof(a) << std::endl;
+
+  // Determine the offset to the index.
+
+
+
+
+  return 0;
+}
--- puzzle4.cpp
+++ puzzle4.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 
 
-
 int main(int argc, char *argv[])
 {
   int *x; 
 
   // Turn int pointer x into a new heap alloc array with size.
 
-
   // Create a int with label size with value 3.
+  
+  int size = 3;
 
+  x = new int[size];
 
   // Write a for loop that loops through size.
   // Printing out its value in each iteration.
 
+  for ( int i = 0; i < size; i++ )
+  {
+    x[i] = i + 1;
+    std::cout << *x << std::endl;
+  }
 
   // Memory management:: delete malloc array and remove
   // dangling pointer.
+ 
+  delete[] x;
+  x = nullptr;
 
 
   return 0;
